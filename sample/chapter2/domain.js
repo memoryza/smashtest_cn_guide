57b652a4ca15c6072dd7ea5fe0f33c418aed8f36
memoryza <jincai.wang@foxmail.com>
2019-09-27 11:59:24 +0800

    docz: delete demo

--- sample/chapter2/domain.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-const server = require('http').createServer(app);
-
-app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
-});
-server.listen(3000);
